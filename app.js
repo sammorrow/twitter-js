b48a9df7f552d42e0c190b8ed719f00cdea7c835
Sam Morrow <smorrow@oberlin.edu>
2017-04-25 11:17:18 -0400

    starting build of routes

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const volleyball = require('volleyball')
+const nunjucks = require('nunjucks')
 const app = express();
 
 
-var status;
+//nunjucks
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+var main = function() {
+    nunjucks.render('index.html', locals, function(err, output) {
+        console.log(output);
+    });
+}
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf' },
+        { name: 'Frodo' },
+        { name: 'Hermione' }
+    ]
+};
+
+//express
 
 app.use(volleyball); //first call: before following func, last call: after
-
 app.use("/", function(req, res, next) {
     /*res.on("testEvent", () => {
         console.log(res.statusCode)
     }) */ //jury-rigged implementation
     console.log(req.method, req.url);
     //res.emit("testEvent") //adds the eventemitter to the test queue
+    main();
     next();
 });
 
@@ -23,6 +42,11 @@ app.use("/special/", function(req, res, next) {
 
 app.get("/", function(req, res) {
     res.status(200).send("welcome!");
+});
+
+app.get("/views/index.html", function(req, res) {
+    //res.sendfile('./views/index.html') //untemplated, raw file
+    res.render('index.html', locals);
 });
 
 const PORT = 3001;
